0a7722af77bdbec97235295be43cc3e5a3c9096c
Nathan <145821197+eunathan098@users.noreply.github.com>
2025-01-13 23:23:52 -0300

    Criação da página de lanchonete

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+document.getElementById('menuToggle').addEventListener('click', function() {
+    const menu = document.querySelector('.navbar .menu');
+    menu.classList.toggle('active');
+  });
+  
